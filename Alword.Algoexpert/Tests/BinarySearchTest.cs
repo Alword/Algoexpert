b7609196b7db90d62ce612e78ecedac056561aa4
Alword <anzer987@yandex.ru>
2020-08-13 09:59:42 +0300

    feature binary tree search

---
+++ Alword.Algoexpert/Tests/BinarySearchTest.cs
@@ -0,0 +1,29 @@
+﻿using Alword.Algoexpert.Tier0;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alword.Algoexpert.Tests
+{
+    public class BinarySearchTest
+    {
+        [Fact]
+        public static void TestSearchEven()
+        {
+            var array = new int[] { 1, 5, 23, 111 };
+            var target = 111;
+            var index = 3;
+            Assert.Equal(index, BinarySearchTask.BinarySearch(array, target));
+        }
+
+        [Fact]
+        public static void TestSearchOdd()
+        {
+            var array = new int[] { 0, 1, 21, 33, 45, 45, 61, 71, 72, 73 };
+            var target = 33;
+            var index = 3;
+            Assert.Equal(index, BinarySearchTask.BinarySearch(array, target));
+        }
+    }
+}
---
+++ Alword.Algoexpert/Tier0/BinarySearchTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier0
+{
+    public class BinarySearchTask
+    {
+        public static int BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+            while (!(left > right))
+            {
+                int index = (left + right) / 2;
+                if (target > array[index])
+                    left = index + 1;
+                else if (target < array[index])
+                    right = index - 1;
+                else
+                    return index;
+            }
+            return -1;
+        }
+    }
+}
