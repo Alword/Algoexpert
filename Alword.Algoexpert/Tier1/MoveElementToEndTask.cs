0f02567b495fe02fbbfc35489b52aedb428b334a
Alword <anzer987@yandex.ru>
2020-08-13 17:54:54 +0300

    feature move element to end task

---
+++ Alword.Algoexpert/Tier1/MoveElementToEndTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier1
+{
+    public class MoveElementToEndTask
+    {
+        public static List<int> MoveElementToEnd(List<int> array, int toMove)
+        {
+            int left = 0;
+            int right = array.Count - 1;
+            while (left < right)
+            {
+                while (array[right] == toMove && right > left)
+                    right--;
+                while (array[left] != toMove && right > left)
+                    left++;
+
+                var temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+            return array;
+        }
+    }
+}
