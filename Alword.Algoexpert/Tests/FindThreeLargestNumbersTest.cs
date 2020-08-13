7ed025fe356cee1cb6cfbe39dc5abde368ecf2a6
Alword <anzer987@yandex.ru>
2020-08-13 10:19:15 +0300

    FindThreeLargestNumbers

---
+++ Alword.Algoexpert/Tests/FindThreeLargestNumbersTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alword.Algoexpert.Tests
+{
+    public class FindThreeLargestNumbersTest
+    {
+        [Fact]
+        public static void SortTest()
+        {
+            var arr = new List<int> { 1, 2, 3 };
+            arr.Sort();
+        }
+    }
+}
---
+++ Alword.Algoexpert/Tier0/FindThreeLargestNumbersTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier0
+{
+    public class FindThreeLargestNumbersTask
+    {
+        public static int[] FindThreeLargestNumbers(int[] array)
+        {
+            List<int> maxs = new List<int>(3) { array[0], array[1], array[2] };
+            maxs.Sort();
+            int min = maxs[0];
+            for (int i = 3; i < array.Length; i++)
+            {
+                if (array[i] > min)
+                {
+                    maxs.RemoveAt(0);
+                    if (array[i] > maxs[0] || array[i] > maxs[1])
+                    {
+                        if (array[i] > maxs[1])
+                            maxs.Add(array[i]);
+                        else
+                            maxs.Insert(1, array[i]);
+                    }
+                    else
+                        maxs.Insert(0, array[i]);
+                    
+                    min = maxs[0];
+                }
+            }
+            return maxs.ToArray();
+        }
+    }
+}
