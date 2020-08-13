3c29d656e78e9726bae851d2d726096ff9e77f76
Alword <anzer987@yandex.ru>
2020-08-13 11:52:14 +0300

    insertion sort

---
+++ Alword.Algoexpert/Tests/InsertionSortTest.cs
@@ -0,0 +1,21 @@
+﻿using Alword.Algoexpert.Tier0;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alword.Algoexpert.Tests
+{
+    public class InsertionSortTest
+    {
+        [Fact]
+        public static void InsertionSort()
+        {
+            var array = new int[] { 8, 5, 2, 9, 5, 6, 3 };
+            var expected = new int[] { 8, 5, 2, 9, 5, 6, 3 };
+            Array.Sort(expected);
+            var result = InsertionSortTask.InsertionSort(array);
+            Assert.Equal(expected, array);
+        }
+    }
+}
---
+++ Alword.Algoexpert/Tier0/InsertionSortTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier0
+{
+    public class InsertionSortTask
+    {
+        public static int[] InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j] < array[j - 1])
+                    {
+                        var temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
