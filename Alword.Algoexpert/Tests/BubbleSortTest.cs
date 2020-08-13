58242cddb2b2370fff25752d223dd1db05facdef
Alword <anzer987@yandex.ru>
2020-08-13 11:26:33 +0300

    bubble sort

---
+++ Alword.Algoexpert/Tests/BubbleSortTest.cs
@@ -0,0 +1,32 @@
+﻿using Alword.Algoexpert.Tier0;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alword.Algoexpert.Tests
+{
+    public class BubbleSortTest
+    {
+        [Fact]
+        public static void SwapTest()
+        {
+            int[] array = new int[] { 1, 2, 3, 4 };
+
+            (array[0], array[1]) = (array[1], array[0]);
+
+            Assert.Equal(2, array[0]);
+            Assert.Equal(1, array[1]);
+        }
+        [Fact]
+        public static void SortTest()
+        {
+            int[] array = new int[] { 8, 5, 2, 9, 5, 6, 3 };
+            int[] sorted = new int[] { 2, 3, 5, 5, 6, 8, 9 };
+
+            BubbleSortTask.BubbleSort(array);
+
+            Assert.Equal(sorted, array);
+        }
+    }
+}
---
+++ Alword.Algoexpert/Tier0/BubbleSortTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier0
+{
+    public class BubbleSortTask
+    {
+        public static int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                bool isSorted = true;
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    int k = j + 1;
+                    if (array[j] > array[k])
+                    {
+                        isSorted = false;
+                        (array[j], array[k]) = (array[k], array[j]);
+                    }
+                }
+                if (isSorted)
+                    break;
+            }
+            return array;
+        }
+    }
+}
