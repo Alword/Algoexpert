f11d1f392cc3e765fe0585597f3953967fb100de
Alword <anzer987@yandex.ru>
2020-08-16 20:30:03 +0300

    feature spiral traverse

---
+++ Alword.Algoexpert/Tests/1/SpiralTraverseTest.cs
@@ -0,0 +1,25 @@
+﻿using Alword.Algoexpert.Tier1;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Alword.Algoexpert.Tests._1
+{
+    public class SpiralTraverseTest
+    {
+        [Fact]
+        public void QuickTest()
+        {
+            int[,] array = new int[,] { { 1, 2, 3, 4 }, { 12, 13, 14, 5 }, { 11, 16, 15, 6 }, { 10, 9, 8, 7 } };
+            int[] expected = Enumerable.Range(1, 16).ToArray();
+            Assert.Equal(expected, SpiralTraverseTask.SpiralTraverse(array));
+        }
+        [Fact]
+        public void SingleElemTest()
+        {
+            int[,] array = new int[,] { { 1 }};
+            int[] expected = Enumerable.Range(1, 1).ToArray();
+            Assert.Equal(expected, SpiralTraverseTask.SpiralTraverse(array));
+        }
+    }
+}
---
+++ Alword.Algoexpert/Tier1/SpiralTraverseTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier1
+{
+    public class SpiralTraverseTask
+    {
+        public static List<int> SpiralTraverse(int[,] array)
+        {
+            int n1 = 0;
+            int n = array.GetLength(0);
+            int m1 = 0;
+            int m = array.GetLength(1);
+            int total = m * n;
+            List<int> result = new List<int>(total);
+
+            m -= 1;
+            n -= 1;
+            while (result.Count < total)
+            {
+                for (int j = m1; j <= m; j++)
+                {
+                    result.Add(array[n1, j]);
+                }
+                for (int i = n1 + 1; i <= n; i++)
+                {
+                    result.Add(array[i, m]);
+                }
+                for (int j = m - 1; j >= m1; j--)
+                {
+                    if (n1 == n) break;
+                    result.Add(array[n, j]);
+                }
+                for (int i = n - 1; i > n1; i--)
+                {
+                    if (m1 == m) break;
+                    result.Add(array[i, m1]);
+                }
+                n1++;
+                m1++;
+                n--;
+                m--;
+            }
+            return result;
+        }
+    }
+}
