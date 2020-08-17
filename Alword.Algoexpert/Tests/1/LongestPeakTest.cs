41f551b8ce27dde838d7771eb5e68f3fd77af842
Alword <anzer987@yandex.ru>
2020-08-17 19:56:00 +0300

    longest peak

---
+++ Alword.Algoexpert/Tests/1/LongestPeakTest.cs
@@ -0,0 +1,34 @@
+﻿using Alword.Algoexpert.Tier1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alword.Algoexpert.Tests._1
+{
+    public class LongestPeakTest
+    {
+        [Fact]
+        public void TestPeak()
+        {
+            var array = new int[] { 1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3 };
+            int expected = 6;
+            Assert.Equal(expected, LongestPeakTask.LongestPeak(array));
+        }
+        [Fact]
+        public void EmptyTest()
+        {
+            var array = new int[] { };
+            int expected = 0;
+            Assert.Equal(expected, LongestPeakTask.LongestPeak(array));
+        }
+
+        [Fact]
+        public void InlineTest()
+        {
+            var array = new int[] { 1, 3, 2 };
+            int expected = 3;
+            Assert.Equal(expected, LongestPeakTask.LongestPeak(array));
+        }
+    }
+}
---
+++ Alword.Algoexpert/Tier1/LongestPeakTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier1
+{
+    public class LongestPeakTask
+    {
+        public static int LongestPeak(int[] array)
+        {
+            int i = 0;
+            int max = 0;
+            while (i < array.Length - 1)
+            {
+                int current = 0;
+                while (i < array.Length - 1 && array[i] < array[i + 1])
+                {
+                    i++;
+                    current++;
+                    if (i < array.Length - 1 && array[i] > array[i + 1])
+                    {
+                        while (i < array.Length - 1 && array[i] > array[i + 1])
+                        {
+                            i++;
+                            current++;
+                        }
+                        if (max < current)
+                        {
+                            max = current + 1;
+                            i--;
+                        }
+                        break;
+                    }
+                }
+                i++;
+            }
+
+            return max;
+        }
+    }
+}
