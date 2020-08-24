51f6d93ab01ec2da86234b0e6532fee94248f07c
Alword <anzer987@yandex.ru>
2020-08-24 13:31:15 +0300

    feature InvertBinaryTree

---
+++ Alword.Algoexpert/Tier1/InvertBinaryTreeTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alword.Algoexpert.Tier1
+{
+    public class InvertBinaryTreeTask
+    {
+        public static void InvertBinaryTree(BinaryTree tree)
+        {
+            Queue<BinaryTree> swapQueue = new Queue<BinaryTree>();
+
+            swapQueue.Enqueue(tree);
+            while (swapQueue.Any())
+            {
+                var swapRoot = swapQueue.Dequeue();
+                if (swapQueue == null) continue;
+                if (swapRoot.left != null)
+                    swapQueue.Enqueue(swapRoot.left);
+                if (swapRoot.right != null)
+                    swapQueue.Enqueue(swapRoot.right);
+                var left = swapRoot.left;
+                swapRoot.left = swapRoot.right;
+                swapRoot.right = left;
+            }
+        }
+        public static void Swap()
+        {
+
+        }
+
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
