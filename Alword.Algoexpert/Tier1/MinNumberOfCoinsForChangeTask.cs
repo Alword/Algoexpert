292823ccf3ccc66bf487a5108d22acf8740b099b
Alword <anzer987@yandex.ru>
2020-08-25 17:40:47 +0300

    feature min numberof coins

---
+++ Alword.Algoexpert/Tier1/MinNumberOfCoinsForChangeTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Alword.Algoexpert.Tier1
+{
+    public class MinNumberOfCoinsForChangeTask
+    {
+        public static int MinNumberOfCoinsForChange(int n, int[] denoms)
+        {
+            int[] counts = new int[n + 1];
+            Array.Fill(counts, int.MaxValue - 1);
+            counts[0] = 0;
+
+            foreach (var denom in denoms)
+            {
+                for (int amount = denom; amount < n + 1; ++amount)
+                {
+                    counts[amount] = Math.Min(counts[amount], counts[amount - denom] + 1);
+                }
+            }
+            return counts[n] == int.MaxValue - 1 ? -1 : counts[n];
+        }
+    }
+}
